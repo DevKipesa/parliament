710c6a3be09926030d2d324adedcbe88ad68f8c6
Dominic Kimani <kymdommie@gmail.com>
2023-06-27 08:55:32 +0300

    Adding a few pages

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import Homepage from './Components/Homepage';
 import Registration from './Components/Registration';
 import Login from './Components/Login';
 import CommitteeReports from './Components/CommitteeReports';
-
+import Announcements from './Components/Announcements';
+import ReviewRoom from './Components/ReviewRoom';
+import Directory from './Components/Directory';
+import DocumentLibrary from './Components/DocumentLibrary';
+import ActionItems from './Components/ActionItems';
+import LiveSession from './Components/LiveSession';
+import Voting from './Components/Voting';
+import HouseBusiness from './Components/HouseBusiness';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Registration />} />
+        <Route path="/" element={<Registration/>} />
         <Route path="/Homepage" element={<Homepage />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/CommitteeReports" element={<CommitteeReports />} />
+        <Route path="/Announcements" element={<Announcements />} />
+        <Route path="/ReviewRoom" element={<ReviewRoom/>} />
+        <Route path="/Directory" element={<Directory />} />
+        <Route path="/DocumentLibrary" element={<DocumentLibrary />} />
+        <Route path="/ActionItems" element={<ActionItems />} />
+        <Route path="/LiveSession" element={<LiveSession/>} />
+        <Route path="/Voting" element={<Voting />} />
+        <Route path="/HouseBusiness" element={<HouseBusiness />} />
+        
       </Routes>
     </Router>
     
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import  { useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBuilding, faVoteYea, faBroadcastTower, faTasks, faFolderOpen, faUsers, faSearch, faBullhorn, faClipboardList } from '@fortawesome/free-solid-svg-icons';
 import './Homepage.css';
+import logo from '../images/logo.png';
 
 const Homepage = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -14,51 +15,76 @@ const Homepage = () => {
     // Display the corresponding contents based on the search term
     // You can update the state or navigate to a different page as needed
   };
+
   return (
     <div>
-      <div className='Dashboard'>
-    <div className="search-box">
-      <input type="text" placeholder="Search" 
-      value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button className="search-button" onClick={handleSearch}>
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-    </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faBuilding} /> House Business</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faVoteYea} /> Voting</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faBroadcastTower} /> Live Session</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faTasks} /> Action Items</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faFolderOpen} /> Document Library</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faUsers} /> Directory</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faSearch} /> Review Room</h3>
-        </div>
-        <div className="module">
-          <h3><FontAwesomeIcon icon={faBullhorn} /> Announcements</h3>
-        </div>
-        <div className="module">
-        <Link className='Link' to="/CommitteeReports">
-          <h3><FontAwesomeIcon icon={faClipboardList} /> Committee Reports</h3>
-          </Link>
+      <div className="background-images">
+        <div className='Dashboard'>
+        <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
         </div>
         
-        
+          <div className="search-box">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button className="search-button" onClick={handleSearch}>
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
+          <div className="module">
+          <Link className='Link' to="/HouseBusiness">
+            <h3><FontAwesomeIcon icon={faBuilding} /> House Business</h3>
+            </Link>
+          </div>
+          <div className="module">
+          <Link className='Link' to="/Voting">
+            <h3><FontAwesomeIcon icon={faVoteYea} /> Voting</h3>
+            </Link>
+          </div>
+          <div className="module">
+          <Link className='Link' to="/LiveSession">
+            <h3><FontAwesomeIcon icon={faBroadcastTower} /> Live Session</h3>
+            </Link>
+          </div>
+          <div className="module">
+            <Link className='Link' to="/ActionItems">
+            <h3><FontAwesomeIcon icon={faTasks} /> Action Items</h3>
+            </Link>
+          </div>
+          <div className="module">
+            <Link className='Link' to="/DocumentLibrary">
+            <h3><FontAwesomeIcon icon={faFolderOpen} /> Document Library</h3>
+            </Link>
+          </div>
+          <div className="module">
+            <Link className='Link' to="Directory">
+            <h3><FontAwesomeIcon icon={faUsers} /> Directory</h3>
+            </Link>
+          </div>
+          <div className="module">
+            <Link className='Link' to="/ReviewRoom">
+            <h3><FontAwesomeIcon icon={faSearch} /> Review Room</h3>
+            </Link>
+          </div>
+          <div className="module">
+            < Link className='Link' to="/Announcements">
+            <h3><FontAwesomeIcon icon={faBullhorn} /> Announcements</h3>
+            </Link>
+          </div>
+          <div className="module">
+            <Link className='Link' to="/CommitteeReports">
+              <h3><FontAwesomeIcon icon={faClipboardList} /> Committee Reports</h3>
+            </Link>
+          </div>
+          <div className='mission'>
+          <h3>MISSION: TO REPRESENT THE INTERESTS OF THE KENYAN PEOPLE, ENACT LAWS, AND OVERSEE THE ACTIONS OF THE GOVERNMENT.<br/> VISION: TO BE AN EFFECTIVE AND INDEPENDENT LEGISLATURE THAT PROMOTES DEMOCRACY, UPHOLDS THE RULE OF LAW, AND SAFEGUARDS THE RIGHTS AND WELFARE OF THE PEOPLE OF KENYA.</h3>
+        </div>
+        </div>
       </div>
-      
     </div>
   );
 };
