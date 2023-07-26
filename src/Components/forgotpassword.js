7a3dc11853a5a9e292a9f7119dcb47bd1052172a
Dominic Kimani <kymdommie@gmail.com>
2023-07-26 14:25:34 +0300

    removing icons

--- src/Components/forgotpassword.js
+++ src/Components/forgotpassword.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './forgotpassword.css';
 import { Link } from 'react-router-dom';
 
