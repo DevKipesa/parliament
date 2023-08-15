983bcc232de60114b8c8ef4bd6080a0bcaec2812
Dominic Kimani <kymdommie@gmail.com>
2023-08-15 11:16:17 +0300

    Few edits

--- src/Components/card1.js
+++ src/Components/card1.js
@@ -6,21 +6,23 @@ function card1() {
   return (
     <div>
         < div className='card1'>
+           < div  className='lat'>
+            <h3>LATEST SESSION</h3>
+            </div>
 
-            <h3 className='latest'>LATEST SESSION</h3>
           < div className='house'>
             <h2>< FontAwesomeIcon icon={faSun} className='fasun' /> Morning Session</h2>
-            <h2>< FontAwesomeIcon icon={faCalendar} className='facalendar' /> Mon, March 20th 2023</h2>
-            <h2 > < FontAwesomeIcon icon={faClock} className='faclock' /> 03/20/2023</h2>
+            <h2>< FontAwesomeIcon icon={faCalendar} className='fasun' /> Mon, March 20th 2023</h2>
+            <h2 > < FontAwesomeIcon icon={faClock} className='fasun' /> 03/20/2023</h2>
           </div>
           < div className='commitee'>
-            <h2>< FontAwesomeIcon icon={faSun} className='fasun' /> Afternoon Session</h2>
+            <h2>< FontAwesomeIcon icon={faSun} className='facalendar' /> Afternoon Session</h2>
             <h2>< FontAwesomeIcon icon={faCalendar} className='facalendar' /> Mon,March 27th 2023</h2>
-            <h2>< FontAwesomeIcon icon={faClock} className='faclock' /> 03/27/2023</h2>
+            <h2>< FontAwesomeIcon icon={faClock} className='facalendar' /> 03/27/2023</h2>
           </div>
           <div className='joint'>
-            <h2>< FontAwesomeIcon icon={faUsers} className='fausers' /> Joint Health And Budget</h2>
-            <h2>< FontAwesomeIcon icon={faCalendar} className='facalendar' /> Thur, March 30th 2023</h2>
+            <h2>< FontAwesomeIcon icon={faUsers} className='faclock' /> Joint Health And Budget</h2>
+            <h2>< FontAwesomeIcon icon={faCalendar} className='faclock' /> Thur, March 30th 2023</h2>
             <h2>< FontAwesomeIcon icon={faClock} className='faclock' /> 03/30/2023</h2>
           </div>
       </div>
