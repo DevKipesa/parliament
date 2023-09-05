25e2ae134a242eecc50c0655203fa390c4d521e9
Dominic Kimani <kymdommie@gmail.com>
2023-09-05 15:48:12 +0300

    adding live session code

--- src/Components/LiveSession.js
+++ src/Components/LiveSession.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function LiveSession() {
+  const sessionDetails = {
+    date: 'September 10, 2023',
+    time: '3:00 PM - 4:30 PM',
+  };
+  const handleAttendMeeting =()=>{
+    console.log("Attend meeting clicked")
+  }
+
   return (
-    <div>LiveSession</div>
+    <div>
+      < div className='session'>
+    <h2 className='details'>Session Details</h2>
+    <p>Date: {sessionDetails.date}</p>
+    <p>Time: {sessionDetails.time}</p>
+   </div>
+   < div className='button'>
+   <a href="https://meet.google.com/" target="_blank" rel="noopener noreferrer">
+    <button onClick={handleAttendMeeting}>Attend Meeting</button>
+    </a>
+   </div>
+    </div>
   )
 }
 
